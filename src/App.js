3e0f5262784d3dce430949a1b04c8144d07ab12c
aveWorld <texnarb228@gmail.com>
2021-02-16 16:30:07 +0200

    remove auth

--- src/App.js
+++ src/App.js
@@ -3,21 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 // Styles
 import './styles/Main.scss';
 
-// Components
-import Layout from './components/Layout/Layout';
-import PrivateRoute from './components/PrivateRoute/PrivateRoute';
-
 // Pages
 import Home from './pages/Home/Home';
-import Login from './pages/Login/Login';
 
 function App() {
   return (
     <Switch>
-      <Route exact path="/login" component={() => <Login />} />
-      <Layout>
-        <PrivateRoute exact path="/" component={Home} />
-      </Layout>
+      <Route exact path="/" component={() => <Home />} />
     </Switch>
   );
 }
